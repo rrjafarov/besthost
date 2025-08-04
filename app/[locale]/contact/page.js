c72fa05c17a1768836271fa88bbbdbbac251ec94
rrjafarov <rafael.cefer.4050@gamil.com>
2025-08-04 17:26:24 +0400

    change contact

---
+++ app/[locale]/contact/page.js
@@ -0,0 +1,16 @@
+import Contact from '@/components/ContactPage/Contact'
+import Footer from '@/components/Footer/Footer'
+import Header from '@/components/Header/Header'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <Header />
+        <Contact />
+        <Footer />
+    </div>
+  )
+}
+
+export default page
